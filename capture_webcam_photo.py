a976af96c2cfd650e29a3a8a08f5bbe5e6741df3
CJ <110461799+LearningWithCJ@users.noreply.github.com>
2024-08-14 14:00:30 +0330

    Add files via upload

---
+++ capture_webcam_photo.py
@@ -0,0 +1,27 @@
+#
+#  _                                _                __          __ _  _    _        _____      _ 
+# | |                              (_)               \ \        / /(_)| |  | |      / ____|    | |
+# | |      ___   __ _  _ __  _ __   _  _ __    __ _   \ \  /\  / /  _ | |_ | |__   | |         | |
+# | |     / _ \ / _` || '__|| '_ \ | || '_ \  / _` |   \ \/  \/ /  | || __|| '_ \  | |     _   | |
+# | |____|  __/| (_| || |   | | | || || | | || (_| |    \  /\  /   | || |_ | | | | | |____| |__| |
+# |______|\___| \__,_||_|   |_| |_||_||_| |_| \__, |     \/  \/    |_| \__||_| |_|  \_____|\____/ 
+#                                              __/ |                                              
+#                                             |___/                         -  By CJ
+#
+# YouTube : www.youtube.com/@LearningWithCJ
+# GitHub  : www.github.com/Carl-Johnson1976
+# Telegram: t.me/LearningWithCJ
+#
+
+import cv2
+
+
+
+dst_path = "your_file_path/your_file_name.png"
+cam = cv2.VideoCapture(0)
+
+if cam.isOpened(): # check webcam is available or not
+
+    ret, frame = cam.read()
+    cv2.imwrite(dst_path, frame)
+    cam.release()
